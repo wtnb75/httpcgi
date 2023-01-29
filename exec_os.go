b7568b9957b538d1dda002a4c8ab2843fdc36188
Watanabe Takashi <wtnb75@gmail.com>
2023-01-29 22:24:24 +0900

    lintをCIでチェック

--- exec_os.go
+++ exec_os.go
@@ -10,10 +10,12 @@ import (
 	"golang.org/x/exp/slog"
 )
 
+// OsRunner is normal CGI executor
 type OsRunner struct {
 }
 
-func (runner *OsRunner) getPipe(cmd *exec.Cmd) (stdin io.WriteCloser, stdout io.ReadCloser, stderr io.ReadCloser, err error) {
+func (runner *OsRunner) getPipe(cmd *exec.Cmd) (
+	stdin io.WriteCloser, stdout io.ReadCloser, stderr io.ReadCloser, err error) {
 	if stdin, err = cmd.StdinPipe(); err != nil {
 		slog.Error("stdin", err)
 		return
@@ -29,12 +31,14 @@ func (runner *OsRunner) getPipe(cmd *exec.Cmd) (stdin io.WriteCloser, stdout io.
 	return
 }
 
-func (runner *OsRunner) Run(conf SrvConfig, cmdname string, envvar map[string]string, stdin io.ReadCloser, stdout io.Writer, stderr io.Writer) error {
+// Run implements Runner.Run
+func (runner *OsRunner) Run(conf SrvConfig, cmdname string, envvar map[string]string,
+	stdin io.ReadCloser, stdout io.Writer, stderr io.Writer) error {
 	fn := filepath.Join(conf.BaseDir, cmdname)
 	slog.Debug("path", "full-path", fn)
 	cmd := exec.Command(fn)
 	slog.Debug("pid", "process", cmd.Process)
-	cmd_stdin, cmd_stdout, cmd_stderr, err := runner.getPipe(cmd)
+	cmdStdin, cmdStdout, cmdStderr, err := runner.getPipe(cmd)
 	if err != nil {
 		slog.Error("pipe error", err)
 		return err
@@ -49,13 +53,13 @@ func (runner *OsRunner) Run(conf SrvConfig, cmdname string, envvar map[string]st
 	slog.Debug("pid", "process", cmd.Process)
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go DoPipe(stdin, cmd_stdin, &wg)
+	go DoPipe(stdin, cmdStdin, &wg)
 	wg.Add(1)
-	go DoPipe(cmd_stderr, stderr, &wg)
+	go DoPipe(cmdStderr, stderr, &wg)
 	wg.Add(1)
-	go DoPipe(cmd_stdout, stdout, &wg)
+	go DoPipe(cmdStdout, stdout, &wg)
 	wg.Wait()
-	cmd_stdin.Close()
-	cmd_stderr.Close()
+	cmdStdin.Close()
+	cmdStderr.Close()
 	return nil
 }
