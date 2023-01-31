bef763f48afadf9629c49b497015a8f5e75b20dc
Watanabe Takashi <wtnb75@gmail.com>
2023-01-31 22:09:50 +0900

    wasmer/wasmtime test

---
+++ exec_wasmer_test.go
@@ -0,0 +1,48 @@
+//go:build wasmer
+// +build wasmer
+
+package main
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestWasmerOpenError(t *testing.T) {
+	t.Parallel()
+	runner := &WasmerRunner{}
+	conf := SrvConfig{}
+	fname := "test.wasm"
+	stdin := io.NopCloser(bytes.NewBufferString(""))
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	envvar := map[string]string{}
+	err := runner.Run(conf, fname, envvar, stdin, stdout, stderr)
+	if err == nil {
+		t.Error("no-error")
+	}
+	t.Log("err", err)
+}
+
+func TestWasmerHello(t *testing.T) {
+	t.Parallel()
+	runner := &WasmerRunner{}
+	conf := SrvConfig{BaseDir: "examples"}
+	fname := "hello.wasm"
+	stdin := io.NopCloser(bytes.NewBufferString(""))
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	envvar := map[string]string{}
+	err := runner.Run(conf, fname, envvar, stdin, stdout, stderr)
+	if err != nil {
+		t.Errorf("error %s", err)
+	}
+	if stderr.String() != "finished(stderr).\n" {
+		t.Errorf("stderr %s", stderr.String())
+	}
+	if !strings.HasPrefix(stdout.String(), "Content-Type:") {
+		t.Errorf("stdout %s", stdout.String())
+	}
+}
