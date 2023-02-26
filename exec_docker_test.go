40c5a2bb1a7704fb447c1c487a18bd286decd741
Watanabe Takashi <wtnb75@gmail.com>
2023-02-26 20:22:01 +0900

    docker runのunittest

--- exec_docker_test.go
+++ exec_docker_test.go
@@ -4,11 +4,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/golang/mock/gomock"
 	"github.com/wtnb75/httpcgi/mock_client"
+	"io"
 	"testing"
 )
 
@@ -87,6 +90,37 @@ func TestDockerExistsAPIError(t *testing.T) {
 	}
 	if err == nil {
 		t.Error("no err", err)
+	}
+	return
+}
+
+func TestDockerRun(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	cli := mock_client.NewMockAPIClient(ctrl)
+	runner := DockerRunner{cli: cli}
+	conf := SrvConfig{}
+	conf.DockerMounts = []string{"dir_from1:dir_to2,ro", "dir_from2:dir_to2", "tmp:tmp,tmpfs"}
+	envs := map[string]string{"hello": "world"}
+	stdin := io.NopCloser(bytes.NewBufferString("hello"))
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cres := container.CreateResponse{ID: "id123"}
+	cli.EXPECT().ContainerCreate(gomock.Any(), gomock.Any(), gomock.Any(), nil, nil, "").Return(cres, nil)
+	cli.EXPECT().ContainerRemove(gomock.Any(), "id123", gomock.Any()).Return(nil)
+	cli.EXPECT().ContainerStart(gomock.Any(), "id123", gomock.Any()).Return(nil)
+	ch_exit := make(chan container.WaitResponse, 1)
+	ch_err := make(chan error, 1)
+	cli.EXPECT().ContainerWait(gomock.Any(), "id123", container.WaitConditionNotRunning).Return(ch_exit, ch_err)
+	buf := []byte{}
+	output := io.NopCloser(bytes.NewBuffer(buf))
+	cli.EXPECT().ContainerLogs(gomock.Any(), "id123", gomock.Any()).Return(output, nil)
+	ch_exit <- container.WaitResponse{}
+	// ch_err <- fmt.Errorf("hello %s", "world")
+	err := runner.Run(conf, "path1", envs, stdin, stdout, stderr, context.Background())
+	if err != nil {
+		t.Error("err", err)
 	}
 	return
 }
