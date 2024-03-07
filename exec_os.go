a96fcfd2c3fd11d3b9f5c15da177d60246ed8fa7
Watanabe Takashi <wtnb75@gmail.com>
2024-03-07 23:41:47 +0900

    kill return value

--- exec_os.go
+++ exec_os.go
@@ -80,7 +80,9 @@ func (runner *OsRunner) Run(conf SrvConfig, cmdname string, envvar map[string]st
 	}()
 	if timeoutWait(&wg, conf.Timeout){
 		slog.Warn("timeout")
-		cmd.Process.Kill()
+		if err := cmd.Process.Kill(); err != nil{
+			slog.Error("kill failed", "error", err)
+		}
 		return fmt.Errorf("timeout %v", conf.Timeout)
 	}
 	return nil
